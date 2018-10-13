56eaa745a13d36a9d6454a25166c088d3f00113c
Ryan Pisuena <Pisuenaryan@gmail.com>
2018-10-13 02:36:44 -0700

    feat(add) Now can view

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// app_id = 5FP2Mb897kNdkg0oU5lG
-// app_code = ych9vG1PjklksZ5TFN2HNA
-/*
-var platform = new H.service.Platform({
-  'app_id': '{YOUR_APP_ID}',
-  'app_code': '{YOUR_APP_CODE}'
-});
-*/
 
 var platform = new H.service.Platform({
   'app_id': '5FP2Mb897kNdkg0oU5lG',
@@ -14,34 +6,57 @@ var platform = new H.service.Platform({
 
 var targetElement = document.getElementById("containerOfOurMap");
 
-// Obtain the default map types from the platform object:
-var defaultLayers = platform.createDefaultLayers();
-
-// Instantiate (and display) a map object:
-var map = new H.Map(
-  document.getElementById('containerOfOurMap'),
-  defaultLayers.normal.map,
-  {
-    zoom: 15,
-    center: { lat: 32.8853304, lng: -117.23951698320596}
+// Instantiate a map inside the DOM element with id map. The
+// map center is in San Francisco, the zoom level is 10:
+var map = new H.Map(document.getElementById('containerOfOurMap'),
+  platform.createDefaultLayers().normal.map, {
+  center: {lat: 37.7942, lng: -122.4070},
+  zoom: 15
   });
 
-map.addLayer(defaultLayers.venues);
-map.addLayer(defaultLayers.incidents);
+// Create a group object to hold map markers:
+var group = new H.map.Group();
 
-// Define a variable holding SVG mark-up that defines an icon image:
-var svgMarkup = '<svg width="24" height="24" ' +
-  'xmlns="http://www.w3.org/2000/svg">' +
-  '<rect stroke="white" fill="#1b468d" x="1" y="1" width="22" ' +
-  'height="22" /><text x="12" y="18" font-size="12pt" ' +
-  'font-family="Arial" font-weight="bold" text-anchor="middle" ' +
-  'fill="white">H</text></svg>';
+// Create the default UI components:
+var ui = H.ui.UI.createDefault(map, platform.createDefaultLayers());
 
-// Create an icon, an object holding the latitude and longitude, and a marker:
-var icon = new H.map.Icon(svgMarkup),
-  coords = {lat: 32.8853304, lng: -117.23951698320596},
-  marker = new H.map.Marker(coords, {icon: icon});
+// Add the group object to the map:
+map.addObject(group);
 
-// Add the marker to the map and center the map at the location of the marker:
-map.addObject(marker);
-map.setCenter(coords);
+// Obtain a Search object through which to submit search
+// requests:
+var search = new H.places.Search(platform.getPlacesService()),
+  searchResult, error;
+
+// Define search parameters:
+var params = {
+// Plain text search for places with the word "hotel"
+// associated with them:
+  'q': 'car repair',
+//  Search in the Chinatown district in San Francisco:
+  'at': '37.7942,-122.4070'
+};
+
+// Define a callback function to handle data on success:
+function onResult(data) {
+  addPlacesToMap(data.results);
+}
+
+// Define a callback function to handle errors:
+function onError(data) {
+  error = data;
+}
+
+// This function adds markers to the map, indicating each of
+// the located places:
+function addPlacesToMap(result) {
+  group.addObjects(result.items.map(function (place) {
+  var marker = new H.map.Marker({lat: place.position[0],
+    lng: place.position[1]})
+  return marker;
+  }));
+}
+
+// Run a search request with parameters, headers (empty), and
+// callback functions:
+search.request(params, {}, onResult, onError);
