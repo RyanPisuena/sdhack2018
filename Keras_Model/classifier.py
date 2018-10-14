1a0898e0f3a8f5c2dc0f87145a64bf696703a138
Cameron Podd <podd@usc.edu>
2018-10-14 02:49:07 -0700

    Some more changes to classifier model

--- Keras_Model/classifier.py
+++ Keras_Model/classifier.py
@@ -15,9 +15,8 @@ class keras_model:
   # Setup
   def __init__(self):
     np.random.seed(1234)
-    GOOD_CAR = 0
-    DAMAGED_CAR = 1
     self.model = Sequential()
+    self.__setup_environment()
 
   # Load in 28 x 28 ppm files
   def load_data(self, data_directory):
@@ -34,13 +33,43 @@ class keras_model:
         labels.append(int(d))
     return images, labels
 
+  def predict(self, input):
+    images28 = [transform.resize(image, (28, 28)) for image in input]
+    length = len(images28)
+    my_ary = np.zeros((length, 28, 28))
+    for i, image in enumerate(images28):
+      if image.shape == (28,28):
+        my_ary[i,:,:] = image
+      elif image.shape == (28,28,3):
+        my_ary[i,:,:] = rgb2gray(image)
+      else:
+        print("Problem with data.")
+        return
+    images28 = my_ary
+    model_input = images28.reshape(images28.shape[0], 1, 28, 28)
+    return self.model.predict_classes(model_input)
+
+###################Private Functions############################################
+
+  # Set up model
+  def __SetUpModel(self):
+    self.model.add(Conv2D(32, 3, 3, activation='relu', input_shape=(1, 28, 28),
+      dim_ordering='th'))
+    self.model.add(Conv2D(32, 3, 3, activation='relu'))
+    self.model.add(MaxPooling2D(pool_size=(2,2)))
+    self.model.add(Dropout(0.25))
+    self.model.add(Flatten())
+    self.model.add(Dense(128, activation='relu'))
+    self.model.add(Dropout(0.5))
+    self.model.add(Dense(2, activation='softmax'))
+    self.model.compile(loss='categorical_crossentropy',
+      optimizer='adam', metrics=['accuracy'])
+
   # Set up the environment and runs the convolutional neural network
-  def setup_environment(self):
+  def __setup_environment(self):
     # Load images into a good and damaged numpy array
     PATH = "C:\\Users\\camer\\Documents\\Repositories\\SDHacks2018\\Keras_Model"
     TRAIN_DIR = os.path.join(PATH, "Training")
-    TEST_DIR = os.path.join(PATH, "Testing")
-    # jpgtoppm(TRAIN_DIR)
     images, labels = self.load_data(TRAIN_DIR)
     images28 = [transform.resize(image, (28, 28)) for image in images]
     length = len(images28)
@@ -58,22 +87,3 @@ class keras_model:
     self.__SetUpModel()
     labels = np_utils.to_categorical(labels, 2)
     self.model.fit(input, labels, batch_size=32, nb_epoch=10, verbose=1)
-
-  def predict(self, input):
-    return self.model.predict_classes(input)
-
-###################Private Functions############################################
-
-  # Set up model
-  def __SetUpModel(self):
-    self.model.add(Conv2D(32, 3, 3, activation='relu', input_shape=(1, 28, 28),
-      dim_ordering='th'))
-    self.model.add(Conv2D(32, 3, 3, activation='relu'))
-    self.model.add(MaxPooling2D(pool_size=(2,2)))
-    self.model.add(Dropout(0.25))
-    self.model.add(Flatten())
-    self.model.add(Dense(128, activation='relu'))
-    self.model.add(Dropout(0.5))
-    self.model.add(Dense(2, activation='softmax'))
-    self.model.compile(loss='categorical_crossentropy',
-      optimizer='adam', metrics=['accuracy'])
--- Keras_Model/main.py
+++ Keras_Model/main.py
@@ -1,25 +1,27 @@
 from classifier import keras_model
 import os
 from PIL import Image
+from skimage import data
 
 def main():
   # Load images into a good and damaged numpy array
   PATH = "C:\\Users\\camer\\Documents\\Repositories\\SDHacks2018\\Keras_Model"
   TRAIN_DIR = os.path.join(PATH, "Training")
   TEST_DIR = os.path.join(PATH, "Testing")
-  jpgtoppm(os.path.join(TRAIN_DIR, "1"))
-  #jpgtoppm(TEST_DIR)
+  jpgtoppm(TRAIN_DIR)
   kmodel = keras_model()
-  kmodel.setup_environment()
-
+  broken = data.imread(TRAIN_DIR + "\\Broken2.ppm")
+  car = data.imread("Fixed.ppm")
+  print(kmodel.predict([broken]))
+  print(kmodel.predict([car]))
 
 # Create 28 x 28 ppm files out of differently size jpgs
 def jpgtoppm(data_directory):
-    file_names = [os.path.join(data_directory, f) for f in data_directory
-      if f.endswith(".ppm")]
+    file_names = [data_directory + "\\Broken.jpg"]
     for f in file_names:
-      image = Image.open(f)
-      image.save("11{}.ppm".format(f[0:-4]))
+      image = Image.open(os.path.join(data_directory, f))
+      newimage = image.resize((28, 28))
+      newimage.save(data_directory + "\\Broken2.ppm")
 
 if __name__ == "__main__":
   main()
